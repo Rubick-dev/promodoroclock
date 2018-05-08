2683bbe5d651e2cc54e152d7912b31da25130f14
Heath Burton <rubickdev@gmail.com>
2018-05-08 17:16:43 +1000

    first days of work completed

--- app.js
+++ app.js
@@ -1,29 +1,120 @@
+var timeCounterBreak = 5;
+var timeCounterActive= 25;
+var button = document.getElementById("time-change-button");
+var stopstart = document.getElementById("stop-start");
+var start = "Start";
+var stop = "Stop";
 
+// event Listener for time change buttons(4)
+button.addEventListener('click', changeTimer, false);
 
+function changeTimer(e){
+  if(e.target !== e.currentTarget){
+    let val = e.target.value;
+  
+    switch (val){
 
+      case "1":
+        if (timeCounterBreak > 2){
+          timeCounterBreak -= 1
+          document.querySelector('.break-timer-span').innerHTML = timeCounterBreak;
+        } else {
+            // make the user see that minimum break is 2 minutes.
+        }
+        break;
 
-
-
-// The countdown timer
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  setInterval(function () {
-      minutes = parseInt(timer / 60, 10)
-      seconds = parseInt(timer % 60, 10);
-
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      display.textContent = minutes + ":" + seconds;
-
-      if (--timer < 0) {
-          timer = duration;
+      case "2":
+        if (timeCounterBreak < 99){
+        timeCounterBreak += 1
+        document.querySelector('.break-timer-span').innerHTML = timeCounterBreak;
+        } else {
+          // make the user see that minimum break is 99 minutes.
+        }
+        break;
+     
+      case "3":
+      if (timeCounterActive > 2){
+        timeCounterActive -= 1
+        document.querySelector('.active-timer-span').innerHTML = timeCounterActive;
+      } else {
+        // make the user see that minimum break is 2 minutes.
       }
-  }, 1000);
+      break;
+
+      case "4":
+        if (timeCounterActive < 99){
+          timeCounterActive += 1
+          document.querySelector('.active-timer-span').innerHTML = timeCounterActive;
+        } else {
+          // make the user see that minimum break is 2 minutes.
+        }
+        break;
+
+    }
+  }
+e.stopPropagation();
+};
+
+// event Listener for the stop / start and reset button
+stopstart.addEventListener('click', ControlTimer, false);
+
+function ControlTimer(e) {
+  console.log(e.target.value);
+  if (e.target.value == 6) {
+    resetClock();
+  } else if (e.target.value == 5){
+    var timeNow = timeCounterActive * 60;
+    var display = document.querySelector('#time');
+    document.querySelector(".startbutton").innerHTML = stop;
+    document.querySelector(".startbutton").setAttribute("value", 7);
+    document.querySelector(".startbutton").setAttribute("class", "startbutton btn btn-danger btn-lg");
+    startTimer(timeNow, display);
+  } else if (e.target.value == 7) {
+    document.querySelector(".startbutton").innerHTML = start;
+    document.querySelector(".startbutton").setAttribute("value", 5);
+    document.querySelector(".startbutton").setAttribute("class", "startbutton btn btn-success btn-lg");
+  }
+
+  e.stopPropagation();
 }
 
-window.onload = function () {
-  var fiveMinutes = 60 * 5,
-      display = document.querySelector('#time');
-  startTimer(fiveMinutes, display);
-};
+// do {
+
+// } while ()
+
+// The counter timer
+  function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+  };
+
+
+// window.onload = function () {
+//   var fiveMinutes = 60 * 5,
+//       display = document.querySelector('#time');
+//   // startTimer(fiveMinutes, display);
+// };
+
+function resetClock(){
+  timeCounterBreak = 5;
+  timeCounterActive= 25;
+  document.querySelector('.active-timer-span').innerHTML = timeCounterActive;
+  document.querySelector('.break-timer-span').innerHTML = timeCounterBreak;
+  document.querySelector(".startbutton").innerHTML = start;
+  document.querySelector(".startbutton").setAttribute("value", 5);
+  document.querySelector(".startbutton").setAttribute("class", "startbutton btn btn-success btn-lg");
+  document.querySelector('#time').innerHTML = "25:00";
+}
