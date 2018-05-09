8071321244b2c7e0375b23eaa0ae3c7ab077dc09
Heath Burton <rubickdev@gmail.com>
2018-05-09 15:08:03 +1000

    last commit right before trying a new style of set interval, major overhaul

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var button = document.getElementById("time-change-button");
 var stopstart = document.getElementById("stop-start");
 var start = "Start";
 var stop = "Stop";
+var timeCountDownClear;
+var countType = true;
 
 // event Listener for time change buttons(4)
 button.addEventListener('click', changeTimer, false);
@@ -36,6 +38,7 @@ function changeTimer(e){
       if (timeCounterActive > 2){
         timeCounterActive -= 1
         document.querySelector('.active-timer-span').innerHTML = timeCounterActive;
+        document.querySelector('#time').innerHTML = timeCounterActive + ":00";
       } else {
         // make the user see that minimum break is 2 minutes.
       }
@@ -45,6 +48,7 @@ function changeTimer(e){
         if (timeCounterActive < 99){
           timeCounterActive += 1
           document.querySelector('.active-timer-span').innerHTML = timeCounterActive;
+          document.querySelector('#time').innerHTML = timeCounterActive + ":00";
         } else {
           // make the user see that minimum break is 2 minutes.
         }
@@ -62,6 +66,7 @@ function ControlTimer(e) {
   console.log(e.target.value);
   if (e.target.value == 6) {
     resetClock();
+
   } else if (e.target.value == 5){
     var timeNow = timeCounterActive * 60;
     var display = document.querySelector('#time');
@@ -69,24 +74,23 @@ function ControlTimer(e) {
     document.querySelector(".startbutton").setAttribute("value", 7);
     document.querySelector(".startbutton").setAttribute("class", "startbutton btn btn-danger btn-lg");
     startTimer(timeNow, display);
+
   } else if (e.target.value == 7) {
     document.querySelector(".startbutton").innerHTML = start;
     document.querySelector(".startbutton").setAttribute("value", 5);
     document.querySelector(".startbutton").setAttribute("class", "startbutton btn btn-success btn-lg");
+    pauseTimer();
   }
 
   e.stopPropagation();
 }
 
-// do {
-
-// } while ()
 
 // The counter timer
   function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    
-    setInterval(function () {
+    console.log(minutes);
+    timeCountDownClear = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -96,25 +100,28 @@ function ControlTimer(e) {
         display.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            timer = duration;
+            timer = timeCounterBreak * 60;
         }
     }, 1000);
   };
 
 
-// window.onload = function () {
-//   var fiveMinutes = 60 * 5,
-//       display = document.querySelector('#time');
-//   // startTimer(fiveMinutes, display);
-// };
-
+// function to reset all things back to page onload status
 function resetClock(){
   timeCounterBreak = 5;
   timeCounterActive= 25;
+  pauseTimer();
   document.querySelector('.active-timer-span').innerHTML = timeCounterActive;
   document.querySelector('.break-timer-span').innerHTML = timeCounterBreak;
   document.querySelector(".startbutton").innerHTML = start;
   document.querySelector(".startbutton").setAttribute("value", 5);
   document.querySelector(".startbutton").setAttribute("class", "startbutton btn btn-success btn-lg");
   document.querySelector('#time').innerHTML = "25:00";
+}
+
+
+// //Pausing the clock
+function pauseTimer(){
+console.log("clearInt ran");
+clearInterval(timeCountDownClear);
 }
